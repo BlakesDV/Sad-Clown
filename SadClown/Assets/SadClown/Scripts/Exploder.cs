db10c3a9c5c78aa8ae897d124cc78d233522bb0f
BlakesDV <dmunoz@cocoschool.mx>
2025-03-25 15:32:20 -0600

    Exploder Script init

---
+++ SadClown/Assets/SadClown/Scripts/Exploder.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Exploder : MonoBehaviour
+{
+    public Transform balloon;
+    public GameObject winPanel;
+    public float speed = 10f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
